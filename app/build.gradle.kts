f0ef589ce36fb6776b865fe5616156be10a4ec96
dhkim <kdh4501@naver.com>
2025-04-08 13:39:17 +0900

    fix material3 의존성 추가, composeBom 추가하면서 material은 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,21 +53,26 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.runtime.android)
-    implementation(libs.androidx.material)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.material3.android)
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0-alpha11")
+    implementation(libs.androidx.ui.tooling.preview.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.tooling.preview.android)
-    implementation(libs.androidx.foundation)
+
+    // Specify the Compose BOM with a version definition
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    implementation(composeBom)
+
+    // Override the BOM version when needed
+    implementation("androidx.compose.animation:animation:1.8.0-rc02")
 }
 java {
     toolchain {

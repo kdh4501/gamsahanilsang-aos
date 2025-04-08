51bbbf187e4546af708ddb4abd187477cb589b8e
dhkim <kdh4501@naver.com>
2025-04-08 13:38:07 +0900

    fix TarkaUI 버전 호환문제로 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
-    implementation(libs.tarkaui)    // UI kit
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.material3)

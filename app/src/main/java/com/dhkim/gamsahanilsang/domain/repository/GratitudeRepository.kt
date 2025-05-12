5a5a0520ef4e35b78daf0bb64ab81cd04e45990a
dhkim <kdh4501@naver.com>
2025-05-12 16:50:35 +0900

    feat 필터기능 추가2 Domain층 Repository 정의

--- app/src/main/java/com/dhkim/gamsahanilsang/domain/repository/GratitudeRepository.kt
+++ app/src/main/java/com/dhkim/gamsahanilsang/domain/repository/GratitudeRepository.kt
@@ -1,6 +1,8 @@
 package com.dhkim.gamsahanilsang.domain.repository
 
 import com.dhkim.gamsahanilsang.domain.entity.GratitudeItem
+import com.dhkim.gamsahanilsang.domain.model.GratitudeFilter
+import kotlinx.coroutines.flow.Flow
 
 interface GratitudeRepository {
     suspend fun saveGratitude(item: GratitudeItem)
@@ -9,4 +11,6 @@ interface GratitudeRepository {
     suspend fun delete(item: GratitudeItem)
     suspend fun deleteAllGratitude()
     suspend fun searchGratitudes(searchQuery: String): List<GratitudeItem>
+    // 필터링된 감사 항목을 가져오는 메서드 추가
+    fun getFilteredGratitudeItems(filter: GratitudeFilter): Flow<List<GratitudeItem>>
 }
